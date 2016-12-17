456c56b7386f0c5cc352a5a5f22df442e29dbb4d
alessiorossotti <alessiorossotti@gmail.com>
2016-12-17 18:44:10 +0100

    active links and css

--- app/scripts/routes/data_model_editor_route.js
+++ app/scripts/routes/data_model_editor_route.js
@@ -5,16 +5,6 @@ App.DataModelEditorRoute = Ember.Route.extend({
 
   model: function() {
 
-    dataHandler: return this.store.find('dataHandler','dH1');
-    prefHandler: return this.store.find('dataHandler','dH1').then(
-      function(dataHandler){
-        return dataHandler.get('prefHandler');
-      }
-    );
-    databaseHandler: return this.store.find('dataHandler','dH1').then(
-      function(dataHandler){
-        return dataHandler.get('databaseHandler');
-      }
-    );
+    return this.store.find('dataHandler','dH1');
   }
 });
